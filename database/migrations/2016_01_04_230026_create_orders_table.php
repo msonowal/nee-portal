8b2e0db2257733841c45d0ec1cfcc373821044fd
Manuj Rajbhor <rajbhor.manuj@gmail.com>
2016-01-05 10:52:48 +0530

    status type set to enum in orders

--- database/migrations/2016_01_04_230026_create_orders_table.php
+++ database/migrations/2016_01_04_230026_create_orders_table.php
@@ -28,7 +28,7 @@ class CreateOrdersTable extends Migration
             $table->string('tansaction_id', 100);
             $table->string('bank_id', 100);
             $table->string('card_type', 100);
-            $table->string('status', 100);
+            $table->enum('status', ['PENDING', 'FAILURE', 'SUCCESS']);
             $table->timestamps();
             $table->foreign('candidate_info_id')->references('id')->on('candidate_info');
         });
