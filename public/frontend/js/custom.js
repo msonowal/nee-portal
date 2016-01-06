9c0b747a9ca9e2855205ef8146a45867e16da825
manash sonowal <manash149@gmail.com>
2016-01-06 23:02:16 +0530

    modified css and jquery validator deaults to properly show errors on material inputs

--- public/frontend/js/custom.js
+++ public/frontend/js/custom.js
@@ -3,3 +3,39 @@ function showError (message) {
 
 	Materialize.toast(message, 10000, 'error');
 }
+
+/*
+$.validator.setDefaults({
+	ignore: [],
+    errorClass: 'invalid',
+    validClass: "valid",
+    errorPlacement: function (error, element) {
+    	$(element)
+            .closest("form")
+            .find("label[for='" + element.attr("id") + "']")
+            .attr('data-error', error.text());
+    },
+    submitHandler: function (form) {
+        console.log('form ok');
+    }
+ });
+*/
+
+$.validator.setDefaults({
+  onkeyup: false,
+  errorClass: 'invalid',
+  validClass: 'valid',
+  submitHandler: function(form) {
+    $(form).find("input[type='submit']").prop('disabled', true);
+    $(form).find("button[type='submit']").addClass("disabled");
+    $(form).find("button[type='submit']").prop('disabled', true);
+    form.submit();
+  },
+  errorPlacement: function(error, element) {
+    error.insertAfter($(element).siblings('label'));
+  }
+});
+
+$("form[class~='validate']").each(function(){
+	$(this).validate();
+});
