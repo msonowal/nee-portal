268d72c6b44902c7a04344e355cccc7cfb97fcd6
Manuj Rajbhor <rajbhor.manuj@gmail.com>
2016-01-10 12:23:34 +0530

    google verification meta tag added

--- resources/views/layouts/public_layout.blade.php
+++ resources/views/layouts/public_layout.blade.php
@@ -10,6 +10,7 @@
   <link href="{{ URL::asset('frontend/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
   <link href="{{ asset("frontend/css/style.css") }}" type="text/css" rel="stylesheet" media="screen,projection"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta name="google-site-verification" content="AJiTML964ODyVFzuI_sEuc8YMQAEPlcEf70uTjRpxMY"/>
 </head>
 <body>
   <header>
